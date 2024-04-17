126b3f3c70f534a4a867c3b5bc22dec714dba7b5
hulahup01 <ilya.deepak@yandex.ru>
2024-04-17 18:36:16 +0300

    lab2 data

---
+++ lab2/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO GROUPS (id, name, c_val) VALUES (1, 'goofy', 2);
+INSERT INTO GROUPS (id, name, c_val) VALUES (2, 'aboba', 2);
+INSERT INTO GROUPS (id, name, c_val) VALUES (3, 'nigba', 2);
+
+
+INSERT INTO STUDENTS (id, name, group_id) VALUES (1, 'Illidan', 1);
+INSERT INTO STUDENTS (id, name, group_id) VALUES (2, 'Jack', 1);
+INSERT INTO STUDENTS (id, name, group_id) VALUES (3, 'Duck', 2);
+INSERT INTO STUDENTS (id, name, group_id) VALUES (4, 'Paul', 2);
+INSERT INTO STUDENTS (id, name, group_id) VALUES (5, 'Leon', 3);
+INSERT INTO STUDENTS (id, name, group_id) VALUES (6, 'Georgy', 3);
